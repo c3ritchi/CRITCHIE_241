0ad36f891d45ee596571c6706a7865097ad37bc0
Cameron Ritchie <c3ritchi@uwaterloo.ca>
2022-11-11 14:57:52 -0500

    lab 4 rtx

---
+++ RTX/Objects/mpu9250.d
@@ -0,0 +1,6 @@
+.\objects\mpu9250.o: Project3\MPU9250.c
+.\objects\mpu9250.o: C:\Software\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\mpu9250.o: Project3\type.h
+.\objects\mpu9250.o: Project3\MPU9250.h
+.\objects\mpu9250.o: Project3\ece_spi.h
+.\objects\mpu9250.o: Project3\delay.h
--- RTX/Objects/uart.d
+++ RTX/Objects/uart.d
@@ -1,4 +1,4 @@
-.\objects\uart.o: Project2\uart.c
+.\objects\uart.o: Project3\uart.c
 .\objects\uart.o: C:\Software\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.7.0\Device\Include\lpc17xx.h
 .\objects\uart.o: C:\Software\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm3.h
 .\objects\uart.o: C:\Software\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
@@ -7,4 +7,4 @@
 .\objects\uart.o: C:\Software\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armcc.h
 .\objects\uart.o: C:\Software\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\mpu_armv7.h
 .\objects\uart.o: C:\Software\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.7.0\Device\Include\system_LPC17xx.h
-.\objects\uart.o: Project2\uart.h
+.\objects\uart.o: Project3\uart.h
